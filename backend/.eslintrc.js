78f90782eee3fba64a726c473e225f7141f4e360
chrystophermedeiros <chrystopher312@gmail.com>
2023-04-23 10:56:04 -0300

    update

---
+++ backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard", "prettier"],
+
+  plugins: ["prettier"],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    camelcase: "off",
+    "prettier/prettier": "0",
+  },
+};
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-
-const routes = new Router()
-
-routes.get('/', (request, response) =>{
-    return response.json({message: "hello World"})
-})
-
-module.exports = routes
